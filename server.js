15406283713ab3414e5647b8dfcaa57644de7a72
unknown <wavsuraj@gmail.com>
2024-07-22 22:46:48 +0530

    Adding Netlify related files

--- server.js
+++ server.js
@@ -12,34 +12,34 @@ const paginatedShowRoute = require('./routes/paginatedShow.js');
 const showRoute = require('./routes/show.js');
 // const getCastDetailsByShowIdRoute = require('./routes/castDetailsByShowId.js');
 
+export async function handler(event, context) {
+
+    mongoose.Promise = global.Promise;
+
+    mongoose.connect(dbConfig.url, {
+        useNewUrlParser: true
+    }).then(() => {
+        console.log("Databse Connected Successfully!!");
+    }).catch(err => {
+        console.log('Could not connect to the database', err);
+        process.exit();
+    });
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
 
-mongoose.Promise = global.Promise;
+    app.use('/getPaginatedShowDetails', rateLimitMiddleware, paginatedShowRoute);
+    app.use('/getAllShowDetails', rateLimitMiddleware, showRoute);
 
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Databse Connected Successfully!!");
-}).catch(err => {
-    console.log('Could not connect to the database', err);
-    process.exit();
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+    // app.use('/getCastDetailsByShowId', rateLimitMiddleware, getCastDetailsByShowIdRoute);
 
 
-app.use('/getPaginatedShowDetails', rateLimitMiddleware, paginatedShowRoute);
-app.use('/getAllShowDetails', rateLimitMiddleware, showRoute);
-
-// app.use('/getCastDetailsByShowId', rateLimitMiddleware, getCastDetailsByShowIdRoute);
-
-
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
-
-app.use("/.netlify/functions/app", router);
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+    // app.use("/.netlify/functions/app", router);
+    return serverless(app)(event, context)
+}
 module.exports.handler = serverless(app);
-
 // module.exports = app;
