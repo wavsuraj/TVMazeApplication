806aee7198a348e5de32f03bf92c93b935ebab3c
unknown <wavsuraj@gmail.com>
2024-07-22 22:57:44 +0530

    netlify changes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const serverless = require('serverless-http');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -12,34 +14,34 @@ const paginatedShowRoute = require('./routes/paginatedShow.js');
 const showRoute = require('./routes/show.js');
 // const getCastDetailsByShowIdRoute = require('./routes/castDetailsByShowId.js');
 
-export async function handler(event, context) {
-
-    mongoose.Promise = global.Promise;
-
-    mongoose.connect(dbConfig.url, {
-        useNewUrlParser: true
-    }).then(() => {
-        console.log("Databse Connected Successfully!!");
-    }).catch(err => {
-        console.log('Could not connect to the database', err);
-        process.exit();
-    });
-
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
 
 
-    app.use('/getPaginatedShowDetails', rateLimitMiddleware, paginatedShowRoute);
-    app.use('/getAllShowDetails', rateLimitMiddleware, showRoute);
+mongoose.Promise = global.Promise;
 
-    // app.use('/getCastDetailsByShowId', rateLimitMiddleware, getCastDetailsByShowIdRoute);
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
-    app.listen(3000, () => {
-        console.log("Server is listening on port 3000");
-    });
-    // app.use("/.netlify/functions/app", router);
-    return serverless(app)(event, context)
-}
+app.use('/getPaginatedShowDetails', rateLimitMiddleware, paginatedShowRoute);
+app.use('/getAllShowDetails', rateLimitMiddleware, showRoute);
+
+// app.use('/getCastDetailsByShowId', rateLimitMiddleware, getCastDetailsByShowIdRoute);
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+
+app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
+
 // module.exports = app;
